136459950622fa13ef1c76c55a88d0e92693d055
MamatovaElnoza <mamatovaelnoza@gmail.com>
2024-10-19 22:03:15 +0300

    improved

--- js.js
+++ js.js
@@ -1,30 +1,29 @@
-const colorBox = document.getElementById('colorBox');
-const changeColorBtn = document.getElementById('changeColorBtn')
+// const colorBox = document.getElementById('colorBox');
+// const changeColorBtn = document.getElementById('changeColorBtn')
 
-changeColorBtn.addEventListener('click', function () {
-    if (colorBox.classList == 'red-background') {
-        colorBox.classList.remove('red-background');
-        colorBox.classList.add('blue-background');
-    } else {
-        colorBox.classList.remove('blue-background');
-        colorBox.classList.add('red-background');
-    }
-});
-
-
-
-
-
-
-// const colorBox = document.getElementById  ('colorBox')
-// const changeColorBtn = document.getElementById  ('changeColorBtn')
-
-// function changeColor() {
-//     if (colorBox.classList == 'redBackground') {
-//         console.log('dfshfg');
-        
-        
-//         colorBox.classList.remove('redBackground');
-//         colorBox.classList.add('blueBackground');
+// changeColorBtn.addEventListener('click', function () {
+//     if (colorBox.classList == 'red-background') {
+//         colorBox.classList.remove('red-background');
+//         colorBox.classList.add('blue-background');
+//     } else {
+//         colorBox.classList.remove('blue-background');
+//         colorBox.classList.add('red-background');
 //     }
-// }
+// });
+
+
+
+
+
+
+const colorBox = document.getElementById('colorBox');
+
+function changeColor() {
+    if (colorBox.classList.contains('redBackground')) {
+        colorBox.classList.remove('redBackground');
+        colorBox.classList.add('blueBackground');
+    } else {
+        colorBox.classList.remove('blueBackground');
+        colorBox.classList.add('redBackground');
+    }
+}

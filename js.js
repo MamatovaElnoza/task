27a69a04e8a052f5b705ec9ce28816c5f94c13d1
MamatovaElnoza <mamatovaelnoza@gmail.com>
2024-10-19 21:14:48 +0300

    simple version

--- js.js
+++ js.js
@@ -1,20 +1,30 @@
-function setBgGreen() {
-    document.body.style.backgroundColor = 'green';
-}
+const colorBox = document.getElementById('colorBox');
+const changeColorBtn = document.getElementById('changeColorBtn')
 
-function setBgRed() {
-    document.getElementsByClassName('red').classList.add('myClass');
-}
-function setBgBlue() {
-    document.body.style.backgroundColor = 'blue';
-}
+changeColorBtn.addEventListener('click', function () {
+    if (colorBox.classList == 'red-background') {
+        colorBox.classList.remove('red-background');
+        colorBox.classList.add('blue-background');
+    } else {
+        colorBox.classList.remove('blue-background');
+        colorBox.classList.add('red-background');
+    }
+});
 
-function setBgYellow() {
-    document.body.style.backgroundColor = 'yellow';
-}
 
-function defaultBgColor() {
-    document.body.style.backgroundColor = 'white';
-}
 
-function addNewClass(elem) { elem.className = "newClass"; }
+
+
+
+// const colorBox = document.getElementById  ('colorBox')
+// const changeColorBtn = document.getElementById  ('changeColorBtn')
+
+// function changeColor() {
+//     if (colorBox.classList == 'redBackground') {
+//         console.log('dfshfg');
+        
+        
+//         colorBox.classList.remove('redBackground');
+//         colorBox.classList.add('blueBackground');
+//     }
+// }
